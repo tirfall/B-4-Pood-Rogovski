a8f1c486596f01a25353e18c9c5ca75e194974e3
opilane <opilane@HP-CZC2349HTB>
2023-02-20 13:02:09 +0200

    fff

--- B_4_Pood_Rogovski.py
+++ B_4_Pood_Rogovski.py
@@ -1,28 +1,44 @@
-from poodmodul import *
+#from poodmodul import *
 
-ostud=[]
-hinnad=[]
-ostnud=[]
-ostnudh=[]
+#ostud=[]
+#hinnad=[]
+#ostnud=[]
+#ostnudh=[]
 
-print("Tere!")
-pood(ostud,hinnad)
-while True:
-    try:
-        choice=int(input("0 - vaata ostud ja hinnad\n1 - Koosta tšekk\n2 - Näitab tähestikulises järjekorras\n3 - Leia kõige kallim/odavam kaup\n4 - Leia hind\n5 - Clear nimekirja ja lisa andmed\n6 - exit\n"))
-        if choice == 0:
-            print(ostud,"\n",hinnad)
-        elif choice == 1:
-            koostamine(ostud,hinnad,ostnud,ostnudh)
-        elif choice == 2:
-            sorteerimine(ostud,hinnad)
-        elif choice == 3:
-            kaloda(ostud,hinnad)
-        elif choice == 4:
-            hinaleia(ostud,hinnad)
-        elif choice == 5:
-            tahtleia(ostud,hinnad)
-        elif choice == 6:
-            break
-    except ValueError:
-        print("vale andmetüüp!")
+#print("Tere!")
+#pood(ostud,hinnad)
+#while True:
+#    try:
+#        choice=int(input("0 - vaata ostud ja hinnad\n1 - Koosta tšekk\n2 - Näitab tähestikulises järjekorras\n3 - Leia kõige kallim/odavam kaup\n4 - Leia hind\n5 - Clear nimekirja ja lisa andmed\n6 - exit\n"))
+#        if choice == 0:
+#            print(ostud,"\n",hinnad)
+#        elif choice == 1:
+#            koostamine(ostud,hinnad,ostnud,ostnudh)
+#        elif choice == 2:
+#            sorteerimine(ostud,hinnad)
+#        elif choice == 3:
+#            kaloda(ostud,hinnad)
+#        elif choice == 4:
+#            hinaleia(ostud,hinnad)
+#        elif choice == 5:
+#            tahtleia(ostud,hinnad)
+#        elif choice == 6:
+#            break
+#    except ValueError:
+#        print("vale andmetüüp!")
+
+#A = [13, 34, 4, 46, 53, 6, 63]
+
+#summa = 0
+
+#for x in A:
+
+#  if x % 2 != 0:
+
+#    summa += x
+
+#print ( summa )
+
+#print([2, 1, 4, 1].count(1))
+a = [1, 2, 3, 4, 5]
+print('Last element of list is', a[-1])
